1a582049743cc3272140a024f08027437b5fdd3b
itsmiraz <mirajulislamofficial044@gmail.com>
2024-08-18 14:46:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <HeartIcon />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
