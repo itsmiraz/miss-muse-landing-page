2a838888a015e4de271440e1dd10911bcdfa48cb
itsmiraz <mirajulislamofficial044@gmail.com>
2024-08-25 12:38:24 +0600

    glow animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useScroll, useSpring, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 import About from "./components/home/about";
 import Expertise from "./components/home/expertise";
 import Hero from "./components/home/hero";
@@ -6,56 +6,56 @@ import Receive from "./components/home/recieve";
 import StartNow from "./components/home/startNow";
 import Training from "./components/home/training";
 import Footer from "./components/ui/footer";
-import {motion } from 'framer-motion'
 import Header from "./components/ui/header";
-import PinkBlob from './assets/images/hero/pink-glow.png'
-import BlueGlowImg from './assets/images/hero/blue-glow.png'
 
-
+import MergedGlows from './assets/images/hero/combined glow-lg.png'
+import MergedGlowsSM from './assets/images/hero/combined-glow-sm.png'
 
 
 function App() {
-  const { scrollYProgress } = useScroll();
-  const springConfig = {
-    stiffness: 100,
-    damping: 20,
-    mass: 1,
-  };
-  const rotateEllipsis1 = useTransform(scrollYProgress, [0, 1], [0, -360]);
-  const rotateEllipsis2 = useTransform(scrollYProgress, [0, 1], [0, 360]);
-  const springRotateEllipsis1 = useSpring(rotateEllipsis1, springConfig);
-  const springRotateEllipsis2 = useSpring(rotateEllipsis2, springConfig);
+  
   return (
     <main className="max-w-[2000px] relative overflow-hidden transition-all ease-in-out duration-300 mx-auto">
-    <div className="relative z-30">
-    <Header />
-
-<Hero />
- <Expertise />
-<Training />
-<Receive />
-<StartNow />
-<About />
-<Footer />
-    </div>
-   <motion.div
-        className="fixed w-[1500px]   z-10 -top-[300px] md:-top-[169px] -left-[600px] md:-left-[500px] opacity-25"
-        style={{ rotate: springRotateEllipsis1 }}
-        initial="hidden"
-        animate="visible"
+      <div className="relative z-30">
+        <Header />
+        <Hero />
+        <Expertise />
+        <Training />
+        <Receive />
+        <StartNow />
+        <About />
+        <Footer />
+      </div>
+      <div
+        className="fixed w-[580px] md:w-[1400px] mx-auto h-[600px] md:h-[1000px] z-10 inset-0 flex items-center justify-center   opacity-25"
       >
-        <img src={PinkBlob} className="w-[1000px] md:w-[1102px]" alt="" />
-      </motion.div>
-      <motion.div
-        className="fixed  w-[1500px]  z-10 -top-[0px] -right-[500px] md:-right-[500px] opacity-25"
-        style={{ rotate: springRotateEllipsis2 }}
-        initial="hidden"
-        animate="visible"
-      >
-        <img src={BlueGlowImg} className="w-[1500px]  md:w-[1102px]" alt="" />
-      </motion.div>
+        
+       <motion.div
+       className="md:block hidden"
+      
+        animate={{ rotate: 360 }}  // Rotate 360 degrees
+        transition={{
+          repeat: Infinity,       // Repeat the animation infinitely
+          ease: "linear",         // Use a linear easing function for constant speed
+          duration: 18,           // Set the duration of one full rotation
+        }}>
+       <img src={MergedGlows} className="w-[1402px]" alt="" />
+       </motion.div>
+       <motion.div
+       className="md:hidden block"
+      
+        animate={{ rotate: 360 }}  // Rotate 360 degrees
+        transition={{
+          repeat: Infinity,       // Repeat the animation infinitely
+          ease: "linear",         // Use a linear easing function for constant speed
+          duration: 15,           // Set the duration of one full rotation
+        }}>
+       <img src={MergedGlowsSM} className="w-[500px]" alt="" />
+       </motion.div>
+      </div>
+    
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
